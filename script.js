bd245e4e22a411d23ac72fd7b29e658f98e130a2
akezcom <78246897+akezcom@users.noreply.github.com>
2024-07-06 00:37:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function setFooter(){
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/akezcom" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Akezcom &copy; 2024 <a style="color:gray;text-decoration:none" href="https://www.facebook.com/akezcom" target="_blank"> Contact</a></center>'
+  }
